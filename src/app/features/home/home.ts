6c4e9c204f7f7353a719129efd3f983bb97e85eb
itsswayize <alishumba61@gmail.com>
2026-09-24 15:25:54 +0200

    added animations to the scroll wheel

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -16,9 +16,23 @@ import { PromoModalComponent } from '../../shared/components/promo-modal/promo-m
 export class HomeComponent {
   services$: Observable<Service[]>;
   barbers$: Observable<Barber[]>;
+  showScrollCue = true;
 
   constructor(private readonly businessData: BusinessDataService) {
     this.services$ = this.businessData.getServices();
     this.barbers$ = this.businessData.getBarbers();
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.showScrollCue = window.scrollY < 24;
+  }
+
+  scrollToStandard(event: Event) {
+    event.preventDefault();
+    document.getElementById('standard-section')?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
   }
 }
